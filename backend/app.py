f16734322da3c9c1e369a82d18c0ba34be2ab25e
Marilia <ramonap2013@gmail.com>
2024-03-13 16:52:47 -0500

    install dependencies

--- backend/app.py
+++ backend/app.py
@@ -22,9 +22,27 @@
 # ?>
 
 from flask import Flask
+import jsonify
+import requests
+
 app = Flask(__name__)
+
+MUSEUM_URL = "https://api.artic.edu/api/v1/artworks/search?query[term][is_public_domain]=true&limit=0"
 
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+
+@app.route('/artwork', methods=['GET'])
+def get_artwork():
+    try:
+        response = requests.get(MUSEUM_URL)
+        return jsonify(response.json()), 200
+    except:
+        return jsonify({"message": "An error occurred while processing your request"}), 500
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
